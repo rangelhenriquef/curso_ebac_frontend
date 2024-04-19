9706dd8a62e609f29bd4f0b714eef53a14a31b0e
Rangel <rangel.moura@outlook.com>
2024-04-18 22:50:46 -0300

    Alteração do CCS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,40 @@
 $(document).ready(function(){
-    $('cpf').mask('000.000.000-00', {
-        placeholder: '__/__/____'
-    });
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '999.999.999-99'
+    })
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(99) 99999-9999'
+    })
+    $('#cep').mask('00000-000', {
+        placeholder: '99999-999'
+    })
 
     $('form').validate({
         rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true
+            },
             cpf: {
                 required: true,
                 cpf: true
             },
-            messages: {
-                cpf: 'Informe um CPF válido'
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
             }
+        },
+        messages: {
+            cpf: 'Digite um CPF válido!',
+            telefone: 'Digite um telefone',
+            cep: 'CEP inválido ou inexistente'
         }
     });
 })
